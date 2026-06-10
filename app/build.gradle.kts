05a2e531be8e6bad14c6fa9ec0d6667cdc255eec
ybhgl <zgglybh@gmail.com>
2026-06-10 19:51:53 +0800

    build(config): bump version to 2.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,8 +44,8 @@ android {
         applicationId = "com.ybhgl.reminder"
         minSdk = 28
         targetSdk = 36
-        versionCode = 210
-        versionName = "2.1.0"
+        versionCode = 220
+        versionName = "2.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
